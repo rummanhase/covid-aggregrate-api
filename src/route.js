755ee752349dfc7fc512285b68ec710b111adbc5
rummanhase <rummanhase@gmail.com>
2023-04-25 22:11:10 +0530

    totalRecovered route

--- src/route.js
+++ src/route.js
@@ -1,9 +1,17 @@
 const router = require('express').Router() 
 const { connection } = require('./connector')  
 router.get('/' , async(req , res)=>{
-    console.log('api called');
+    console.log('/ called');
     const data = await connection.find({}) 
     res.send(data)
+})
+
+router.get('/totalRecovered' , async(req , res)=>{
+    console.log('totalRecovered api called');
+    const data = await connection.find({}) 
+    let totalRecovered =0
+    data.map(a=>{totalRecovered += a.recovered})
+    res.send( {data: {_id: "total", recovered:totalRecovered}})
 })
 
 module.exports = router
