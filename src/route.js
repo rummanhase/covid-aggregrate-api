728985909436cc0806516f6f83cf86c4301b7acc
rummanhase <rummanhase@gmail.com>
2023-04-25 22:44:54 +0530

    final route commit

--- src/route.js
+++ src/route.js
@@ -14,4 +14,43 @@ router.get('/totalRecovered' , async(req , res)=>{
     res.send( {data: {_id: "total", recovered:totalRecovered}})
 })
 
+router.get('/totalActive' , async(req , res)=>{
+    console.log('totalActive api called');
+    const data = await connection.find({}) 
+    let totalActive =0
+    data.map(a=>{totalActive += a.infected - a.infected - a.death})
+    res.send( {data: {_id: "total", active:totalActive}})
+})
+
+router.get('/hotspotStates' , async(req , res)=>{
+    console.log('hotspotStates api called');
+    const myData = await connection.find({}) 
+    let data =[]
+    let myRate;
+    myData.map(a=>{
+        myRate = ((a.infected - a.recovered)/a.infected).toFixed(5)
+        if(myRate>0.1){
+            data.push({"state": a.state, "rate": myRate})
+        }
+    })
+
+    res.send( {data})
+})
+
+router.get('/healthyStates' , async(req , res)=>{
+    console.log('healthyStates api called');
+    const myData = await connection.find({}) 
+    let data =[]
+    let myRate;
+    myData.map(a=>{
+        mortality  = (a.death/a.infected).toFixed(5)
+        if(mortality<0.005){
+            data.push({"state": a.state, "mortality": mortality})
+        }
+    })
+
+    res.send( {data})
+})
+
+
 module.exports = router
